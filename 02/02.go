45c98c63f72f086adab483dfb2a47853021a04ca
Brian Wrestler <brian.wrestler@fortrobotics.com>
2025-12-02 21:00:43 -0800

    Day 2 problem 2

--- 02/02.go
+++ 02/02.go
@@ -10,13 +10,58 @@ import (
 
 func main() {
 	var rangeArray = strings.Split(util.ReadInput("./input"), ",")
-	result := processRangeArray(rangeArray)
+	resultProblem1 := processRangeArrayProblem1(rangeArray)
+	resultProblem2 := processRangeArrayProblem2(rangeArray)
 
-	fmt.Printf("The sum of the invalid ids is: %d\n", result)
+	fmt.Printf("[Problem 1] The sum of the invalid ids is: %d\n", resultProblem1)
+	fmt.Printf("[Problem 2] The sum of the invalid ids is: %d\n", resultProblem2)
 }
 
-// Process the range. We will convert each number to a string, split it in half, and then XOR  the halves. If 0 they are the same
-func processRange(start int, end int) int64 {
+func checkArrayEquality(arr []string) bool {
+	for element := range arr {
+		// skip empty elements. SplitAfter can create these
+		if arr[element] != "" && arr[element] != arr[0] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func processRangeProblem2(start int, end int) int64 {
+	var sum int64 = 0
+	for id := start; id <= end; id++ {
+		idStr := strconv.Itoa(id)
+
+		for i := 0; i <= len(idStr)/2 && len(idStr) > 1; i++ {
+			splits := strings.SplitAfter(idStr, string(idStr[0:i]))
+
+			// Make sure they are all equal and over 1 character long
+			if checkArrayEquality(splits) {
+				fmt.Printf("%d is a invalid id\n", id)
+				sum += int64(id)
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func processRangeArrayProblem2(rangeArray []string) int64 {
+	var result int64 = 0
+	for _, idRangeStr := range rangeArray {
+		idRange := strings.Split(idRangeStr, "-")
+		start, _ := strconv.Atoi(idRange[0])
+		end, _ := strconv.Atoi(idRange[1])
+
+		result += processRangeProblem2(start, end)
+	}
+
+	return result
+}
+
+func processRangeProblem1(start int, end int) int64 {
 	var sum int64 = 0
 	for id := start; id <= end; id++ {
 		// Turn into a string so we can split it in half easily
@@ -25,7 +70,7 @@ func processRange(start int, end int) int64 {
 		secondHalf := idStr[len(idStr)/2:]
 
 		if firstHalf == secondHalf {
-			fmt.Printf("%d is an invalid id\n", id)
+			fmt.Printf("%d is a invalid id\n", id)
 			sum += int64(id)
 		}
 	}
@@ -33,14 +78,14 @@ func processRange(start int, end int) int64 {
 	return sum
 }
 
-func processRangeArray(rangeArray []string) int64 {
+func processRangeArrayProblem1(rangeArray []string) int64 {
 	var result int64 = 0
 	for _, idRangeStr := range rangeArray {
 		idRange := strings.Split(idRangeStr, "-")
 		start, _ := strconv.Atoi(idRange[0])
 		end, _ := strconv.Atoi(idRange[1])
 
-		result += processRange(start, end)
+		result += processRangeProblem1(start, end)
 	}
 
 	return result
--- 02/02_test.go
+++ 02/02_test.go
@@ -4,8 +4,7 @@ import (
 	"testing"
 )
 
-func TestProcessRanges_AoC_Example(t *testing.T) {
-
+func TestProcessRanges_AoC_Example_1(t *testing.T) {
 	rangeArray := []string{
 		"11-22",
 		"95-115",
@@ -19,10 +18,61 @@ func TestProcessRanges_AoC_Example(t *testing.T) {
 
 	var want int64 = 1227775554
 
-	result := processRangeArray(rangeArray)
+	result := processRangeArrayProblem1(rangeArray)
 
 	if result != want {
 		t.Errorf("The result was %d but wanted %d", result, want)
 	}
+}
 
+func TestProcessRanges_AoC_Single_Digts(t *testing.T) {
+	rangeArray := []string{
+		"2-12",
+	}
+
+	var want int64 = 11
+
+	result := processRangeArrayProblem2(rangeArray)
+
+	if result != want {
+		t.Errorf("The result was %d but wanted %d", result, want)
+	}
+}
+
+func TestProcessRanges_AoC_Example_2(t *testing.T) {
+	rangeArray := []string{
+		"11-22",
+		"95-115",
+		"998-1012",
+		"1188511880-1188511890",
+		"222220-222224",
+		"1698522-1698528",
+		"446443-446449",
+		"38593856-38593862",
+		"565653-565659",
+		"824824821-824824827",
+		"2121212118-2121212124",
+	}
+
+	var want int64 = 4174379265
+
+	result := processRangeArrayProblem2(rangeArray)
+
+	if result != want {
+		t.Errorf("The result was %d but wanted %d", result, want)
+	}
+}
+
+func TestProcessRanges_AoC_Tripple(t *testing.T) {
+	rangeArray := []string{
+		"121312131213-121312131214",
+	}
+
+	var want int64 = 121312131213
+
+	result := processRangeArrayProblem2(rangeArray)
+
+	if result != want {
+		t.Errorf("The result was %d but wanted %d", result, want)
+	}
 }
